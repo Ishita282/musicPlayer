a679f150dd269b8490f5fc591f643b470d521ddd
Ishita282 <littleminiishita@gmail.com>
2025-11-08 16:36:40 +0530

    server.js

--- server.js
+++ server.js
@@ -1,20 +1,16 @@
-// server.js
 const express = require("express");
 const path = require("path");
 const app = express();
 const PORT = 3000;
 
-// Serve static files (HTML, CSS, JS)
 app.use(express.static(path.join(__dirname, "docs")));
 
-// Serve songs from the /songs directory
 app.use("/songs", express.static(path.join(__dirname, "songs")));
 
-// Default route
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "docs", "index.html"));
 });
 
 app.listen(PORT, () => {
-  console.log(`🎧 Music Player running at http://localhost:${PORT}`);
+  console.log(`Music Player running at http://localhost:${PORT}`);
 });
